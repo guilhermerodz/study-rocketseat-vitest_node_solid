9494398f252151d25a3b4ff08653d6865e69e85d
Guilherme Rodz <gui.rodz.dev@gmail.com>
2022-10-03 00:21:21 +0200

    fix: typo

--- src/repositories/appointments-repository.ts
+++ src/repositories/appointments-repository.ts
@@ -1,6 +1,6 @@
 import { Appointment } from '../entities/appointment'
 
-export interface AppointmentRepository {
+export interface AppointmentsRepository {
   create(appointment: Appointment): Promise<void>
   save(appointment: Appointment): Promise<void>
 }
